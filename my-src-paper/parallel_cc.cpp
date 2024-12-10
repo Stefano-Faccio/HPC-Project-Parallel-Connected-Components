cfb79862f4bfe3251f9bcecc0b70ff6e57cef57a
Stefano Faccio <104690017+Stefano-Faccio@users.noreply.github.com>
2024-12-10 17:00:57 +0100

    Paralled MPI version compile (#1)
     Compilation ok

---
+++ my-src-paper/parallel_cc.cpp
@@ -0,0 +1,49 @@
+#include <mpi.h>
+#include <iostream>
+#include <vector>
+#include "utils/GraphInputIterator.hpp"
+#include "utils/SparseSampling.hpp"
+#include <cstdint>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	if (argc != 2 ) {
+		cout << "Usage: connectivity INPUT_FILE" << endl;
+		return 1;
+	}
+
+	MPI_Init(&argc, &(argv));
+
+	GraphInputIterator input(argv[1]);
+	int32_t rank, p;
+	int32_t seed = 0;
+
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &p);
+
+	
+	SparseSampling sampler(MPI_COMM_WORLD, (int32_t)0, p, seed + rank, (int32_t)1, input.vertexCount(), input.edgeCount());
+	/*
+	sampler.loadSlice(input);
+	SparseSampling iteration_sampler(sampler);
+	*/
+
+	MPI_Barrier(MPI_COMM_WORLD);
+
+	vector<uint32_t> components;
+	int number_of_components = 0;//iteration_sampler.connectedComponents(components);
+
+	if (rank == 0) {
+		cout << fixed;
+		cout << argv[1] << ","
+					<< p << ","
+					<< input.vertexCount() << ","
+					<< input.edgeCount() << ","
+					<< "cc" << ","
+					<< number_of_components << endl;
+	}
+
+	MPI_Finalize();
+}
---
+++ my-src-paper/utils/Edge.hpp
@@ -0,0 +1,77 @@
+#pragma once
+
+#include <iostream>
+#include <tuple>
+#include <mpi.h>
+#include <cstdint>
+
+using namespace std;
+
+struct Edge
+{
+    uint32_t from, to;
+
+    inline bool operator<(Edge const &other) const
+    {
+        return (from < other.from) || ((from == other.from) && (to < other.to));
+    }
+
+    inline bool normalized() const
+    {
+        return from <= to;
+    }
+
+    inline void normalize()
+    {
+        if (!normalized())
+        {
+            swap(from, to);
+        }
+    }
+
+    inline bool operator==(Edge const &other) const
+    {
+        return (from == other.from) && (to == other.to);
+    }
+
+    inline bool operator!=(Edge const &other) const
+    {
+        return !this->operator==(other);
+    }
+
+    friend ostream &operator<<(ostream &out, Edge const &edge)
+    {
+        out << edge.from << " -- " << edge.to;
+        return out;
+    }
+};
+
+static_assert(sizeof(Edge) == 8, "Expecting 4B for a uint32_t");
+
+struct MPIEdge
+{
+    static MPI_Datatype edge_type;
+    static bool initialized;
+
+    static MPI_Datatype constructType()
+    {
+        if (!initialized)
+        {
+            int blocklengths[2] = {1, 1};
+
+            // This leaks abstraction :(
+            MPI_Datatype types[2] = {MPI_UINT32_T, MPI_UINT32_T};
+
+            MPI_Aint offsets[2];
+
+            offsets[0] = offsetof(Edge, from);
+            offsets[1] = offsetof(Edge, to);
+
+            MPI_Type_create_struct(2, blocklengths, offsets, types, &edge_type);
+            MPI_Type_commit(&edge_type);
+
+            initialized = true;
+        }
+        return edge_type;
+    };
+};
