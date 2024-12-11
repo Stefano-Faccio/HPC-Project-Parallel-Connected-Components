e58c90528d7a5b197abb8667b7537a73366597e2
Stefano Faccio <stefano.faccio@studenti.unitn.it>
2024-12-11 15:56:08 +0100

    Redy to test

--- my-src-paper/parallel_cc.cpp
+++ my-src-paper/parallel_cc.cpp
@@ -10,44 +10,48 @@ using namespace std;
 // Seed for the random number generator
 int32_t seed = 17;
 
+void debugPrint(const string &message, int32_t rank)
+{
+	if(rank == 0)
+		cout << "Ciao: " << message << " " << rank << endl;
+}
+
 int main(int argc, char *argv[])
 {
-	cout << "Ciao0" << endl;
 	if (argc != 2)
 	{
 		cout << "Usage: connectivity INPUT_FILE" << endl;
 		return 1;
 	}
-	cout << "Ciao1" << endl;
+
 	// Initialize MPI
 	MPI_Init(&argc, &(argv));
-	cout << "Ciao2" << endl;
 	
 	// Get the rank and size in the original communicator: rank is the process ID, size is the number of processes
 	int32_t rank, group_size;
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &group_size);
-	cout << "Ciao3" << endl;
+	debugPrint("1", rank);
 
 	// Read the number of vertices and edges from the input file
 	GraphInputIterator input(argv[1]);
-	cout << "Ciao4" << endl;
+	debugPrint("2", rank);
 
 	// Create a sampler object in which the graph will be loaded
 	SparseSampling sampler(MPI_COMM_WORLD, (int32_t)0, group_size, seed + rank, (int32_t)1, input.vertexCount(), input.edgeCount());
-	cout << "Ciao5" << endl;
+	debugPrint("3", rank);
 	// Load the appropriate slice of the graph in every process
 	sampler.loadSlice(input);
-	cout << "Ciao6" << endl;
+	debugPrint("4", rank);
 
 	// Wait for all processes to finish loading the graph
 	MPI_Barrier(MPI_COMM_WORLD);
-	cout << "Ciao7" << endl;
+	debugPrint("5", rank);
 
 	// Compute the connected components
 	vector<uint32_t> components;
 	int number_of_components = sampler.connectedComponents(components);
-	cout << "Ciao8" << endl;
+	debugPrint("6", rank);
 
 	// Output the number of connected components if the process is the master
 	if (rank == 0)
@@ -60,9 +64,9 @@ int main(int argc, char *argv[])
 			 << "cc" << ","
 			 << number_of_components << endl;
 	}
-	cout << "Ciao9" << endl;
+	debugPrint("7", rank);
 
 	// Close MPI
 	MPI_Finalize();
-	cout << "Ciao10" << endl;
+	debugPrint("8", rank);
 }
