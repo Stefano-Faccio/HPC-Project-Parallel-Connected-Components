2793abb97e303535f278596ece8dcd8af25259e2
Stefano Faccio <stefano.faccio@studenti.unitn.it>
2024-12-20 11:16:45 +0100

    foo

--- Randomized_CC/randomized_cc.cpp
+++ Randomized_CC/randomized_cc.cpp
@@ -8,7 +8,27 @@
 
 using namespace std;
 
-int main() {
+int main(int argc, char* argv[]) {
+
+    if (argc != 2 ) {
+		cout << "Usage: connectivity INPUT_FILE" << endl;
+		return 1;
+	}
+
+	//Open the file and read the number of vertices and edges
+	GraphInputIterator input(argv[1]);
+	cout << "Vertex count: " << input.vertexCount() << " Edge count: " << input.edgeCount() << endl;
+
+	uint32_t real_edge_count = 0;
+	for (auto edge : input) {
+		//Check that the edge is valid i.e. the nodes are in the graph
+		assert(edge.from < input.vertexCount());
+		assert(edge.to < input.vertexCount());
+		//Check that the edge is not a self loop
+		if (edge.to != edge.from) {
+			real_edge_count++;
+		}
+	}
 
 
     return 0;
