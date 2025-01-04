a92dd364ad77679f7edb56e105db93d595395259
Stefano Faccio <stefano.faccio@studenti.unitn.it>
2025-01-04 11:37:38 +0100

    foo

--- Randomized_CC/randomized_cc.cpp
+++ Randomized_CC/randomized_cc.cpp
@@ -17,13 +17,7 @@
 
 using namespace std;
 
-// Define what type of graph we are using: false for undirected and true for directed
-#define DOUBLE_ARCH false
-
-// The behavior of the random number generator is not defined with openmp
-// Test with different compilers and check if the results are the same
-#define TEST_RANDOM_GEN false
-
+void configure_RNG();
 void coin_toss_and_child_hook(uint32_t nNodes, const vector<Edge>& edges, vector<atomic<uint32_t>>& labels);
 vector<Edge> find_rank_and_remove_edges(uint32_t nNodes, const vector<Edge>& edges, vector<atomic<uint32_t>>& labels);
 void map_results_back(uint32_t nNodes, const vector<Edge>& edges, const vector<atomic<uint32_t>>& labels, vector<atomic<uint32_t>>& map);
@@ -56,12 +50,10 @@ vector<atomic<uint32_t>>& par_randomized_cc(uint32_t nNodes, const vector<Edge>&
 	if(nextEdges.size() == edges.size())
 	{
 		//Print edges
-		cout << "Error (same edges as last iterarion): ";
+		cerr << "Error iteration " << *iteration << " (same edges as last iterarion): ";
 		for(uint32_t i = 0; i < edges.size(); i++)
-		{
-			cout << "[" << edges[i].from << "," << edges[i].to << "] ";
-		}
-		cout << endl;
+			cerr << "[" << edges[i].from << "," << edges[i].to << "] ";
+		cerr << endl;
 	}
 
 	// Recursively call the function
@@ -76,7 +68,6 @@ vector<atomic<uint32_t>>& par_randomized_cc(uint32_t nNodes, const vector<Edge>&
 	return map;
 }
 
-// Coin toss and child hook - OK
 void coin_toss_and_child_hook(uint32_t nNodes, const vector<Edge>& edges, vector<atomic<uint32_t>>& labels) 
 {
 	// Hook child to a parent based on the coin toss		
@@ -102,10 +93,8 @@ void coin_toss_and_child_hook(uint32_t nNodes, const vector<Edge>& edges, vector
 			// Other option is to use a critical section or omp atomic if possible
 			if(coin_toss[from] && !coin_toss[to])
 				labels[from].store(labels[to].load());
-			#if !DOUBLE_ARCH
 			else if(!coin_toss[from] && coin_toss[to])
 				labels[to].store(labels[from].load());
-			#endif
 		}
 	}
 
@@ -135,10 +124,8 @@ vector<Edge> find_rank_and_remove_edges(uint32_t nNodes, const vector<Edge>& edg
 				s[i] = 1;
 		}
 
-		
 		// if __GNUC__ >= 10, use the new omp scan directive
 		// Otherwise, use the old way to do a prefix sum
-
 		#if __GNUC__ >= 10
 		// Prefix sum with parallel for
 		#pragma omp for reduction(inscan, + : temp)
@@ -197,33 +184,21 @@ void map_results_back(uint32_t nNodes, const vector<Edge>& edges, const vector<a
 		// Race condition on labels avoided by using atomic variables
 		if(to == labels[from])
 			map[from].store(map[to]);
-		#if !DOUBLE_ARCH
 		else if(from == labels[to])
 			map[to].store(map[from]);
-		#endif
 	}
 
 }
 
 int main(int argc, char* argv[]) {	
-	// Configure the RNG: seed is private to each thread and persistent across calls
-    #pragma omp parallel
-    {
-		// Seed the random number generator for each thread
-		random_genator = mt19937(977 * omp_get_thread_num());
 
-		#if TEST_RANDOM_GEN
-		// Test the random number generator
-		#pragma omp critical
-		{
-			cout << "Thread " << omp_get_thread_num() << ": ";
-			for (int i = 0; i < 10; i++) {
-				cout << random_genator() << " ";
-			}
-			cout << endl;
-		}
-		#endif
-    }
+	//---------------------- Configure the RNG ----------------------
+
+	// A merseene twister is private to each thread and persistent across calls
+	// Every thread will have a different seed
+	configure_RNG();
+
+	//---------------------- Read the graph ----------------------
 
 	if (argc != 2 ) {
 		cout << "Usage: connectivity INPUT_FILE" << endl;
@@ -235,7 +210,7 @@ int main(int argc, char* argv[]) {
 	cout << "Vertex count: " << input.vertexCount() << " Edge count: " << input.edgeCount() << endl;
 
 	vector<Edge> edges;
-	edges.reserve(input.edgeCount());
+	//edges.reserve(input.edgeCount()); // Probably not necessary
 	uint32_t real_edge_count = 0;
 	for (auto edge : input) {
 		//Check that the edge is valid i.e. the nodes are in the graph
@@ -245,19 +220,17 @@ int main(int argc, char* argv[]) {
 		if (edge.to != edge.from) {
 			edges.push_back(edge);
 			real_edge_count++;
-			#if DOUBLE_ARCH
-			Edge reverse_edge = {edge.to, edge.from};
-			edges.push_back(reverse_edge);
-			real_edge_count++;
-			#endif
 		}
 	}
-	//assert(real_edge_count == input.edgeCount());
-	if(real_edge_count != input.edgeCount())
-	{
-		cout << "Warning: " << input.edgeCount() - real_edge_count << " self loops were removed" << endl;
-	}
 
+	//Check if self loops were removed
+	if(real_edge_count != input.edgeCount())
+		cout << "Warning: " << input.edgeCount() - real_edge_count << " self loops were removed" << endl;
+
+
+	//---------------------- Compute CC ----------------------
+
+	// Initialize the labels
 	vector<atomic<uint32_t>> labels(input.vertexCount());
 	for(uint32_t i = 0; i < input.vertexCount(); i++) {
 		labels[i] = i;
@@ -268,26 +241,70 @@ int main(int argc, char* argv[]) {
 
 	//Start the timer
 	auto start = chrono::high_resolution_clock::now();
+
+	//Compute the connected components
 	vector<atomic<uint32_t>>& map = par_randomized_cc(input.vertexCount(), edges, labels, &iteration);
+
 	//Stop the timer
 	auto end = chrono::high_resolution_clock::now();
+	//Calculate the duration
 	auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
-	//Print the labels
+
+	//Print the labels at the end
 	/*
 	cout << "Labels at end: ";
 	for (uint32_t i = 0; i < input.vertexCount(); i++) {
 		cout  << map[i] << " ";
 	}
-	cout << endl;*/
-	//Count the number of connected components
-	unordered_set<uint32_t> cc;
-	for (uint32_t i = 0; i < input.vertexCount(); i++) {
-		cc.insert(map[i]);
-	}
-	cout << "Connected components: " << cc.size() << endl;
+	cout << endl;
+	*/
 
+	//Count the number of connected components
+	uint32_t number_of_cc = unordered_set<uint32_t>(map.begin(), map.end()).size();
+
+	// Print the results
+	cout << "Connected components: " << number_of_cc << endl;
 	cout << "Execution time: " << duration.count() << " ms" << endl;
 	cout << "Iterations: " << iteration << endl;
 
     return 0;
+}
+
+void configure_RNG()
+{
+	// Prime numbers for the random number generator seed: usefull to have streems of random numbers that are not correlated
+	vector<int> prime_numbers = {3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,89,97,101,103,107,109,113,127,131,137,139,149,151,157,163,167,173,179,181,191,193,197,199,211,223,227,229,233,239,241,251,257,263,269,271,277,281,283,293,307,311,313,317,331,337,347,349,353,359,367,373,379,383,389,397,401,409,419,421,431,433,439,443,449,457,461,463,467,479,487,491,499,503,509,521,523,541,547,557,563,569,571,577,587,593,599,601,607,613,617,619,631,641,643,647,653,659,661,673,677,683,691,701,709,719,727,733,739,743,751,757,761,769,773,787,797,809,811,821,823,827,829,839,853,857,859,863,877,881,883,887,907,911,919,929,937,941,947,953,967,971,977,983,991,997,1009,1013,1019,1021,1031,1033,1039,1049,1051,1061,1063,1069,1087,1091,1093,1097,1103,1109,1117,1123,1129,1151,1153,1163,1171,1181,1187,1193,1201,1213,1217,1223};
+
+    #pragma omp parallel
+    {
+		if((uint32_t)omp_get_thread_num() > prime_numbers.size())
+		{
+			cout << "Error: Not enough prime numbers for the random number generator" << endl;
+			exit(1);
+		}
+		// Seed the random number generator for each thread
+		random_genator = mt19937(prime_numbers[omp_get_thread_num()]);
+    }
+
+	// To test the random number generator
+	#if false
+	vector<vector<uint32_t>> random_numbers(omp_get_max_threads());
+	#pragma omp parallel
+	{
+		// Generate random numbers
+		for (uint32_t i = 0; i < 10; i++) {
+			random_numbers[omp_get_thread_num()].push_back(random_genator());
+		}
+	}
+	// Print the random numbers
+	for (uint32_t i = 0; i < random_numbers.size(); i++) {
+		cout << "Thread " << i << ": ";
+		for (uint32_t j = 0; j < random_numbers[i].size(); j++) {
+			cout << random_numbers[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+	exit(1);
+	#endif
 }
