55704dfd3b59a253bbedf8d548d8e372fdb4c0fd
Stefano Faccio <stefano.faccio@studenti.unitn.it>
2025-01-12 13:17:43 +0100

    foo

--- BFS_DFS/serial_approaches/dfs.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <vector>
-#include <utility>
-
-using namespace std;
-
-pair<int, vector<int>> cc_dfs(vector<vector<int>> graph);
-void dfs(vector<vector<int>> graph, int node, vector<int>& label, int cc_count);
