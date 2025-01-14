591fd73c0e49c09dee856c0d03479b9feffe0e3c
Faccio <stefano.faccio@hpc-head-n1.unitn.it>
2025-01-14 17:17:01 +0100

    FOO

--- Serial/serial_cc.cpp
+++ Serial/serial_cc.cpp
@@ -5,15 +5,25 @@
 #include <iostream>
 #include <chrono>
 #include <cstdint>
+#include <numeric>
 
 using namespace std;
 
+uint32_t calc_serial_cc(uint32_t nNodes, vector<Edge> edges);
+
+int iterations = 100;
+
 int main(int argc, char* argv[])
 {
-	if (argc != 2 ) {
-		cout << "Usage: connectivity INPUT_FILE" << endl;
+	if (argc < 2 ) {
+		cout << "Usage: connectivity INPUT_FILE [ITERATIONS]" << endl;
 		return 1;
 	}
+
+	if (argc > 2) {
+		iterations = atoi(argv[2]);		
+	}
+	cout << "Iterations: " << iterations << endl;
 
 	// ----------------- Read the graph -----------------
 
@@ -33,13 +43,41 @@ int main(int argc, char* argv[])
 			edges.push_back(edge);
 	}
 
-	// ----------------- Count the connected components -----------------
+	// ----------------- Calculate the connected components -----------------
 
+	vector<double> times(iterations, 0);
+	for(int i = 0; i < iterations; i++)
+	{
+		double time = (double)calc_serial_cc(input.vertexCount(), edges) / 1000.0;
+		times[i] = time;
+		cout << time << ", ";
+	}
+		
+
+	double sum = accumulate(times.begin(), times.end(), 0.0);
+	double mean_times = sum / (double)iterations;
+
+	// Print the results
+	cout << fixed;
+	cout << "------------------------------------------------" << endl;
+	cout << "File Name: " << argv[1] << endl;
+	cout << "Number of vertices: " << input.vertexCount() << endl;
+	cout << "Number of edges: " << edges.size() << endl;
+	cout << "Mean time: " << mean_times << " s" << endl;
+	/*
+	cout << "Number of connected components: " << components.size() << endl;
+	cout << "Elapsed time: " << duration_s.count() << " s" << endl;
+	cout << "Elapsed time: " << duration_ms.count() << " ms" << endl;
+	*/
+}
+
+uint32_t calc_serial_cc(uint32_t nNodes, vector<Edge> edges)
+{
 	//Start the timer
 	auto start = chrono::high_resolution_clock::now();
 
 	//Create a disjoint set with all the vertices 
-	DisjointSets<uint32_t> disjoint_set(input.vertexCount());
+	DisjointSets<uint32_t> disjoint_set(nNodes);
 
 	// Unify the vertices of each edge
 	for (auto edge : edges) 
@@ -52,7 +90,7 @@ int main(int argc, char* argv[])
 	auto duration_ms = chrono::duration_cast<chrono::milliseconds>(end - start);
 
 	unordered_map<uint32_t, uint32_t> components;
-	for (uint32_t i = 0; i < input.vertexCount(); i++) {
+	for (uint32_t i = 0; i < nNodes; i++) {
 		uint32_t rep = disjoint_set.find(i);
 		if (components.find(rep) == components.end()) {
 			components.insert(make_pair(rep, 1));
@@ -61,13 +99,5 @@ int main(int argc, char* argv[])
 		}
 	}
 
-	// Print the results
-	cout << fixed;
-	cout << "------------------------------------------------" << endl;
-	cout << "File Name: " << argv[1] << endl;
-	cout << "Number of vertices: " << input.vertexCount() << endl;
-	cout << "Number of edges: " << edges.size() << endl;
-	cout << "Number of connected components: " << components.size() << endl;
-	cout << "Elapsed time: " << duration_s.count() << " s" << endl;
-	cout << "Elapsed time: " << duration_ms.count() << " ms" << endl;
+	return duration_ms.count();
 }
